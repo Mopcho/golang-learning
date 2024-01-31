cbf7cdae8327e5365f3476a173f10c85e06e766e
Mopcho <47301161+Mopcho@users.noreply.github.com>
2024-02-01 01:04:18 +0200

    variadic functions

--- slices/main.go
+++ slices/main.go
@@ -2,8 +2,22 @@ package main
 
 import "fmt"
 
+func variadicSum(nums ...int) int {
+	sum := 0
+
+	for i := 0; i < len(nums); i++ {
+		sum += nums[i]
+	}
+
+	return sum
+}
+
 func main() {
 	mySLice := make([]int, 0, 10)
 
 	fmt.Printf("%v", mySLice)
+
+	sum := variadicSum(1, 2, 3)
+
+	fmt.Printf("%v", sum)
 }
